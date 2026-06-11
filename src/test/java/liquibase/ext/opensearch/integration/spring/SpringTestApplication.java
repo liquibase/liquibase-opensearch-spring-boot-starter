5325fcfcbf342789cfd840b8d5c05e3afb089ef6
Ralph Ursprung <Ralph.Ursprung@avaloq.com>
2026-06-11 11:03:15 +0200

    fix example application for Spring Boot 4.0
    with SB 4 some autoconfig classes were moved around. we missed this in
    the SB 4 upgrade since we didn't run the test application (neither
    manually nor in CI).
    
    to avoid running into this issue again the exclusion has been moved from
    the config file (which is validated at runtime) to the source (which
    would fail to compile).

--- src/test/java/liquibase/ext/opensearch/integration/spring/SpringTestApplication.java
+++ src/test/java/liquibase/ext/opensearch/integration/spring/SpringTestApplication.java
@@ -2,8 +2,9 @@ package liquibase.ext.opensearch.integration.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.data.elasticsearch.autoconfigure.DataElasticsearchAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataElasticsearchAutoConfiguration.class)
 public class SpringTestApplication {
 
     public static void main(String[] args) {
